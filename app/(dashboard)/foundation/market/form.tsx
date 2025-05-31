9d9bbaa850c645c157d5bdc1066ec9ddca1c0d93
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2025-05-31 17:15:47 +0530

    chore: clean up

--- app/(dashboard)/foundation/market/form.tsx
+++ app/(dashboard)/foundation/market/form.tsx
@@ -43,18 +43,6 @@ import { cn } from "@/lib/utils";
 
 import { defaultValues, schema, type Schema } from "./shared";
 
-const languages = [
-  { label: "English", value: "en" },
-  { label: "French", value: "fr" },
-  { label: "German", value: "de" },
-  { label: "Spanish", value: "es" },
-  { label: "Portuguese", value: "pt" },
-  { label: "Russian", value: "ru" },
-  { label: "Japanese", value: "ja" },
-  { label: "Korean", value: "ko" },
-  { label: "Chinese", value: "zh" },
-] as const;
-
 const countries = [
   { label: "United States", value: "en" },
   { label: "France", value: "fr" },
@@ -108,73 +96,6 @@ function F({ onSubmit, isPending }: FormProps) {
             className="flex flex-col h-full"
           >
             <div className="grid flex-1 auto-rows-min gap-6 px-4">
-              <FormField
-                control={form.control}
-                name="language"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Language</FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant="outline"
-                            role="combobox"
-                            className={cn(
-                              "w-[200px] justify-between",
-                              !field.value && "text-muted-foreground",
-                            )}
-                          >
-                            {field.value
-                              ? languages.find(
-                                  (language) => language.value === field.value,
-                                )?.label
-                              : "Select language"}
-                            <IconSelector className="opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-[200px] p-0">
-                        <Command>
-                          <CommandInput
-                            placeholder="Search framework..."
-                            className="h-9"
-                          />
-                          <CommandList>
-                            <CommandEmpty>No framework found.</CommandEmpty>
-                            <CommandGroup>
-                              {languages.map((language) => (
-                                <CommandItem
-                                  value={language.label}
-                                  key={language.value}
-                                  onSelect={() => {
-                                    form.setValue("language", language.value);
-                                  }}
-                                >
-                                  {language.label}
-                                  <IconCheck
-                                    className={cn(
-                                      "ml-auto",
-                                      language.value === field.value
-                                        ? "opacity-100"
-                                        : "opacity-0",
-                                    )}
-                                  />
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </CommandList>
-                        </Command>
-                      </PopoverContent>
-                    </Popover>
-                    <FormDescription>
-                      This is the language that will be used in the dashboard.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
               <FormField
                 control={form.control}
                 name="country"
