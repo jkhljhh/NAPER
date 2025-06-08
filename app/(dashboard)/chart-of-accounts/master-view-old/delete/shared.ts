3fe5fda44403a34561f8152481f6980d4dc6760f
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2025-06-08 17:23:05 +0530

    remove old code

--- app/(dashboard)/chart-of-accounts/master-view-old/delete/shared.ts
+++
@@ -1,9 +0,0 @@
-// Filename: shared.ts
-// Path: @/app/(dashboard)/foundation/market/
-import { z } from "zod";
-
-export const schema = z.object({
-  id: z.coerce.number().int().nonnegative(),
-});
-
-export type Schema = z.infer<typeof schema>;
